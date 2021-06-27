e47d224b73fa0e46763f195ee715d418435fd2c2
unknown <diegowallace2010@gmail.com>
2021-06-27 17:54:30 -0300

    agrego condiciones

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ def NDJ(datos):
     maximo = datos["maximo"]
     p = datos["probabilidad"]
     intentos = 0 
+    else: 
+         fichas -= 1
+            #print("apuesto y pierdo")
+    #print(intentos, fichas)
+    return intentos, fichas
