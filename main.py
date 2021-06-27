7d7194416f3e6c7f6f2a49bd2eaaa46d98154485
Ghariel <arielgarzon495@gmail.com>
2021-06-27 16:35:10 -0300

    subiendo-2

--- main.py
+++ main.py
@@ -2,19 +2,3 @@ import random
 
 datos= {"fichas": 50,"probabilidad": 0.4, "maximo": 300}
 
-def NDJ(datos):
-    fichas = datos["fichas"]
-    maximo = datos["maximo"]
-    p = datos["probabilidad"]
-    intentos = 0 
-    while ((fichas > 0) and (intentos < maximo)):
-        intentos += 1
-        sorteo = random.random()
-        if (p > sorteo):
-            fichas += 1
-            #print("apuesto y gano")
-        else: 
-            fichas -= 1
-            #print("apuesto y pierdo")
-    #print(intentos, fichas)
-    return intentos, fichas
